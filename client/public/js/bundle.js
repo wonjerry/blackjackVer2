9906d7cba70ff077e653f81adbb627d4407f96dc
wonjae <dldnjswo19@gmail.com>
2017-10-25 09:59:48 +0900

    modify layout

--- client/public/js/bundle.js
+++ client/public/js/bundle.js
@@ -8,46 +8,40 @@ function CanvasManager () {
   if (!(self instanceof CanvasManager)) new CanvasManager()
 
   self.renderer = null
-  self.background = null
   self.stage = null
   self.buttonContainer = null
+  self.barContainer = null
   self.chipContainer = null
+  self.cardContainer = null
+  self.messageContainer = null
+
+  self.background = null
   self.cardTextures = []
-  self.messages = []
-  self.statusBar = null
 }
 
 CanvasManager.prototype.init = function () {
   var self = this
 
+  // create canvas
   self.renderer = PIXI.autoDetectRenderer(1000, 800, {transparent: true})
   document.body.appendChild(self.renderer.view)
 
+  // create background
   self.stage = new PIXI.Container()
   self.background = PIXI.Sprite.fromImage('./../image/table.png')
-
   self.stage.addChild(self.background)
 
+  // create status bar
   self.barContainer = new PIXI.Container()
   self.bar = PIXI.Sprite.fromImage('./../image/bar.jpg')
-  self.barContainer.addChild(self.bar)
-
   self.shadow = PIXI.Sprite.fromImage('./../image/shadow.png')
   self.barContainer.x = 0
   self.barContainer.y = 700
-
+  self.barContainer.addChild(self.bar)
   self.barContainer.addChild(self.shadow)
   self.stage.addChild(self.barContainer)
 
-  /*
-  var sprite = PIXI.Sprite.from(cards[12])
-  sprite.anchor.set(0.5)
-  sprite.x = 0
-  sprite.y = 0
-  //sprite.scale.set(1.5)
-  container.addChild(sprite)
-   */
-
+  // slice card image
   PIXI.loader.add('spritesheet', './../image/cards2.json')
     .load(self.onAssetsLoaded.bind(self))
 
@@ -61,49 +55,165 @@ CanvasManager.prototype.init = function () {
   self.chipContainer.setAttribute('class', 'chipContainer')
   self.chipContainer.innerHTML = mustache.render(template['chips'], {})
   document.body.appendChild(self.chipContainer)
-
+  var chips = document.getElementsByClassName('chipContainer')
+  chips[0].addEventListener('click', function () {
+    console.log('hahahahaha')
+  })
 }
 
 CanvasManager.prototype.onAssetsLoaded = function () {
   var self = this
-  //sprite를 생성해서 각각에게 전해 줘야 함으로 texture만 생성 해 둔다
+  // slice card image and save as texture
   for (var i = 1; i <= 52; i++) {
     self.cardTextures.push(PIXI.Texture.fromFrame('card' + i + '.png'))
   }
 
-  self.drawUserMoney(100)
+  /*
+  나중에 container는 각 플레이어에 맞게 구성 될 것이고 위치도 player에 따라 지정 될 것이다
+   */
+
+  self.cardContainer = new PIXI.Container()
+  self.cardContainer.x = 500
+  self.cardContainer.y = 400
+
+  self.stage.addChild(self.cardContainer)
+
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addValueSumView(19, self.cardContainer)
+
+  self.cardContainer = new PIXI.Container()
+  self.cardContainer.x = 300
+  self.cardContainer.y = 360
+
+  self.stage.addChild(self.cardContainer)
+
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addValueSumView(19, self.cardContainer)
+
+  self.cardContainer = new PIXI.Container()
+  self.cardContainer.x = 110
+  self.cardContainer.y = 280
+
+  self.stage.addChild(self.cardContainer)
+
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addValueSumView(19, self.cardContainer)
+
+  self.cardContainer = new PIXI.Container()
+  self.cardContainer.x = 700
+  self.cardContainer.y = 360
+
+  self.stage.addChild(self.cardContainer)
+
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addValueSumView(19, self.cardContainer)
+
+  self.cardContainer = new PIXI.Container()
+  self.cardContainer.x = 890
+  self.cardContainer.y = 280
+
+  self.stage.addChild(self.cardContainer)
+
+  self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  //self.addCard({suit: 0, value: 1}, self.cardContainer)
+  self.addValueSumView(19, self.cardContainer)
+
+  self.drawBetMoney(100)
+  self.drawBalMoney(1000)
+  self.addMessageView("WIN")
 }
 
-CanvasManager.prototype.addStatusInfo = function () {
+/**
+ *
+ * @param card // suit  0 ~ 3 , value  1 ~ 13
+ * @param container // card image container for each player
+ */
+CanvasManager.prototype.addCard = function (card, container) {
   var self = this
 
+  var suit = card.suit
+  var value = card.value
+  var index = suit * 13 + value
+
+  var sprite = PIXI.Sprite.from(self.cardTextures[index])
+  sprite.anchor.set(0.5)
+  sprite.x = 30 * container.children.length
+  sprite.y = 0
+  sprite.scale.set(0.8)
+
+  // 으허 container에 anchor가 안먹어서 내가 직접 밀어줘야하다니....
+  container.x = container.x - 12
+
+  container.addChild(sprite)
 }
 
-CanvasManager.prototype.drawUserMoney = function (money) {
+CanvasManager.prototype.drawBetMoney = function (money) {
   var self = this
 
   var graphics = new PIXI.Graphics()
   // draw a rounded rectangle
   graphics.lineStyle(2, 0xb5beb4, 1)
   graphics.beginFill(0x1C281A, 1)
-  graphics.drawRoundedRect(10, 10, 290, 80, 15)
+  graphics.drawRoundedRect(10, 10, 200, 80, 15)
   graphics.endFill()
 
-  var text = new PIXI.Text('Your Money : $' + money, {
+  var text = new PIXI.Text('BET : $' + money, {
     font: '30px Snippet',
     fill: 'white',
     align: 'center'
   })
 
   text.anchor.set(0.5, 0.5)
-  text.position.set(150, 50)
+  text.position.set(110, 50)
   self.barContainer.addChild(graphics)
   self.barContainer.addChild(text)
 }
 
-CanvasManager.prototype.addMessageView = function (x, y, message) {
+CanvasManager.prototype.drawBalMoney = function (money) {
   var self = this
-  var messageContainer = new PIXI.Container()
+
+  var graphics = new PIXI.Graphics()
+  // draw a rounded rectangle
+  graphics.lineStyle(2, 0xb5beb4, 1)
+  graphics.beginFill(0x1C281A, 1)
+  graphics.drawRoundedRect(790, 10, 200, 80, 15)
+  graphics.endFill()
+
+  var text = new PIXI.Text('BAL : $' + money, {
+    font: '30px Snippet',
+    fill: 'white',
+    align: 'center'
+  })
+
+  text.anchor.set(0.5, 0.5)
+  text.position.set(890, 50)
+  self.barContainer.addChild(graphics)
+  self.barContainer.addChild(text)
+}
+
+CanvasManager.prototype.addMessageView = function (message) {
+  var self = this
+
+  self.messageContainer = new PIXI.Container()
 
   var graphics = new PIXI.Graphics()
   graphics.lineStyle(2, 0xb5beb4, 1)
@@ -120,12 +230,12 @@ CanvasManager.prototype.addMessageView = function (x, y, message) {
   text.anchor.set(0.5, 0.5)
   text.position.set(150, 50)
 
-  messageContainer.addChild(graphics)
-  messageContainer.addChild(text)
+  self.messageContainer.addChild(graphics)
+  self.messageContainer.addChild(text)
+  self.messageContainer.x = 350
+  self.messageContainer.y = 180
 
-  stage.addChild(messageContainer)
-  // 나중에 제거를 위한 것이다
-  self.messages.push(messageContainer)
+  self.stage.addChild(self.messageContainer)
   /*
   setTimeout(function () {
     messageContainer.destroy(true)
@@ -134,15 +244,25 @@ CanvasManager.prototype.addMessageView = function (x, y, message) {
   */
 }
 
-CanvasManager.prototype.addTransparentView = function (x, y, message) {
+CanvasManager.prototype.addValueSumView = function (sum, container) {
   var self = this
 
   var graphics = new PIXI.Graphics()
   // draw a rounded rectangle
-  graphics.beginFill(0xFFFFFF, 0.25)
-  //graphics.drawRoundedRect(10, 540, 280, 100, 15)
-  graphics.drawRoundedRect(100, -50, 100, 100, 15)
+  graphics.beginFill(0x000000, 0.7)
+  graphics.drawRoundedRect(-40, -70, 50, 50, 15)
   graphics.endFill()
+
+  var text = new PIXI.Text(sum, {
+    font: '30px Snippet',
+    fill: 'white',
+    align: 'center'
+  })
+
+  text.anchor.set(0.5, 0.5)
+  text.position.set(-15, -45)
+  container.addChild(graphics)
+  container.addChild(text)
 }
 
 CanvasManager.prototype.renderLoop = function () {
---
+++ client/src/utill.js
@@ -0,0 +1,76 @@
+module.exports = {
+  SUITS: {HEART: 0, DIAMOND: 1, SPADE: 2, CLUB: 3},
+  RANKS: ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'],
+  ACTIONS: {
+    DEAL: 'Deal',
+    DROP: 'Drop',
+    HIT: 'Hit',
+    STAND: 'Stand',
+    DOUBLE: 'Double',
+    SPLIT: 'Split', // TODO: 구현 안됨
+    SURRENDER: 'Surrender', // TODO: 구현 안됨
+    INSURANCE: 'Insurance' // TODO: 구현 안됨
+  },
+  GAMESTATES: {INIT: 'init', BETTING: 'bet', HITTING: 'hit', PROCESSING: 'process'},
+  PLAYERSTATES: {
+    DEAL: 'deal',
+    DROP: 'drop',
+    BLACKJACK: 'blackjack',
+    BUST: 'bust',
+    STAND: 'stand',
+    WIN: 'win',
+    LOSE: 'lose',
+    TIE: 'tie'
+  },
+  DEALER: 'Dealer',
+  /**
+   * Calculates the score total of a blackjack hand.
+   * An ace is treated as 11 until the score is above
+   * 21 then it is used as a 1 instead. Returns an
+   * integer value of the score of the hand.
+   *
+   * @param {Array} cards
+   * @return {Integer} sum
+   */
+  score: function (cards) {
+    var sum = 0
+
+    // A flag to determine whether the hand has an ace
+    var ace = false
+
+    for (var i = 0, value; i < cards.length; i++) {
+      if (!cards[i]) {
+        // throw 'card is null: ' + i
+        debug('card is null: ' + i)
+      }
+
+      value = cards[i].rank
+
+      if (value >= 10) value = 10
+      else if (value === 1) ace = true
+
+      sum += value
+    }
+
+    // Treat the ace as an 11 if the hand will not bust
+    if (ace && sum < 12) sum += 10
+
+    return sum
+  },
+  isNumber (s) {
+    s += '' // 문자열로 변환
+    s = s.replace(/^\s*|\s*$/g, '') // 좌우 공백 제거
+    if (s === '' || isNaN(s)) return false
+    return true
+  },
+  cardContainerXY: [
+    [110, 280],
+    [300, 360],
+    [500, 400],
+    [700, 360],
+    [890, 280]
+  ],
+  messageViewXY : [350,180],
+  valueSumViewXY: [-47,-70],
+  valueSumTextXY: [-15,-45],
+}
